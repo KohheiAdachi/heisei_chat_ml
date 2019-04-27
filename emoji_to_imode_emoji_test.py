56bd6a907d8c8cb0feefe38dc182b72dace62b8b
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2019-04-27 22:32:27 +0900

    全emojiで検証

--- emoji_to_imode_emoji_test.py
+++ emoji_to_imode_emoji_test.py
@@ -3,13 +3,19 @@
 
 import text_to_pkebell as pkebell
 import json
-
+import csv
 
 
 emoji_json_file = open("../emoji-ja/data/emoji_ja.json","r")
 emoji_json = json.load(emoji_json_file)
 
+def write_csv(emoji,word):
+    write_ele = [emoji,word]
+    with open("result_imode_emoji_to_emoji.txt",mode="a",encoding='utf-8') as f:
+        writer = csv.writer(f,lineterminator='\n')
+        writer.writerow(write_ele)
+
 for emoji in emoji_json:
     word = pkebell.emoji_to_imode_emoji(emoji)
-    print(emoji,word)
+    write_csv(emoji,word)
 
